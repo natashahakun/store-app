c30097905a4b1c0b726536645785934a28345a55
natashalynne <hakun.natasha@gmail.com>
2016-01-14 08:34:48 -0500

    Create product prototype.

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+
+//use object constructor to create Product prototype
+var Product = function(name, price, material, weight) {
+  this.name = name;
+  this.price = price;
+  this.material = material;
+  this.weight = weight;
+}
